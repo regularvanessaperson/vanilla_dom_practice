7f26ed39fded69f64b5241a67ee12d1c36f86ef5
regularvanessaperson <vamavila4@gmail.com>
2020-09-07 18:45:58 -0500

    logged class into array

--- dom_practice.js
+++ dom_practice.js
@@ -4,3 +4,6 @@ console.log(image)
 const welcome = document.querySelector('h2')
 welcome.innerText = welcome.innerText + "!!!"
 console.log(welcome)
+
+const first = document.querySelectorAll(".list")
+console.log(first)
