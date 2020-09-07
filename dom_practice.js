1293195550994725145f52367a16c37317c0bb66
regularvanessaperson <vamavila4@gmail.com>
2020-09-07 18:44:35 -0500

    added exclamation at the end of h2

--- dom_practice.js
+++ dom_practice.js
@@ -2,4 +2,5 @@ const image = document.querySelectorAll("#skyline")
 console.log(image)
 
 const welcome = document.querySelector('h2')
+welcome.innerText = welcome.innerText + "!!!"
 console.log(welcome)
